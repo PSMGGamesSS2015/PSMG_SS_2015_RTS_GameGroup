df64a05c7bc893c33ea8e74a81975c3e8db3a700
djb12 <davidbrem@t-online.de>
2015-06-07 10:07:47 +0200

    setup the persistence manager

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/PersistenceManager.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/PersistenceManager.cs
@@ -1,19 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/// <summary>
+/// The persistence manager is a subcomponent of the game manager and
+/// is responsible for storing and loading save games.
+/// </summary>
 
 public class PersistenceManager : MonoBehaviour
 {
+
+    private List<SaveGame> savedGames;
+    private SaveGame currentGame;
 
     private List<PersistenceManagerListener> listeners;
 
     public interface PersistenceManagerListener
     {
-        // TODO
+        void OnGameSaved();
+        void OnGameLoaded();
     }
 
     private void Awake()
     {
+        savedGames = new List<SaveGame>();
         listeners = new List<PersistenceManagerListener>();
     }
 
@@ -27,9 +39,36 @@ public class PersistenceManager : MonoBehaviour
         listeners.Remove(listener);
     }
 
+    public void SaveGame()
+    {
+        savedGames.Add(currentGame);
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream fileStream = File.Create(Application.persistentDataPath + "/savedGames.gd")
+        binaryFormatter.Serialize(fileStream, savedGames);
+        fileStream.Close();
+
+        // TODO wait for fileWriting to be completed
+        foreach (PersistenceManagerListener listener in listeners)
+        {
+            listener.OnGameSaved();
+        }
+    }
+
     public void LoadGame()
     {
-        // TODO
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            savedGames = (List<SaveGame>)binaryFormatter.Deserialize(fileStream);
+            fileStream.Close();
+        }
+
+        // TODO wait for fileReading to be completed
+        foreach (PersistenceManagerListener listener in listeners)
+        {
+            listener.OnGameLoaded();
+        }
     }
 
 }

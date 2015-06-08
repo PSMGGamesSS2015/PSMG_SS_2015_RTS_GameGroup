9b63abef3e0e8a9b2ff7f79c733ef039329a79b2
guj10033 <j.guegel@gmail.com>
2015-06-08 16:23:54 +0200

    SoundReferences added

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/SoundReferences.cs
@@ -0,0 +1,144 @@
+﻿public class SoundReferences
+{
+    #region Theme
+
+    public const string BACKGROUND_THEME = "BackgroundTheme";
+    public const string MAIN_THEME = "MainTheme";
+    public const string WON_THEME = "WonLevel";
+
+    #endregion
+
+    #region Sounds
+
+    public const string BODENSCHALTER = "FX_bodenschalter";
+    public const string BOMB_EXPLOSION = "FX_bombe explodiert";
+    public const string BUSH_BURN = "FX_busch abbrennen";
+    public const string BUSH_RUSTLE1 = "FX_buschrascheln1";
+    public const string BUSH_RUSTLE2 = "FX_buschrascheln2";
+    public const string TORCH_LIGHT1 = "FX_fackel entzuenden1";
+    public const string TORCH_LIGHT2 = "FX_fackel entzuenden2";
+    public const string SHIELD_WOOD1 = "FX_holzschild1";
+    public const string SHIELD_WOOD2 = "FX_holzschild2";
+    public const string SHIELD_METAL1 = "FX_metallschild1";
+    public const string SHIELD_METAL2 = "FX_metallschild2";
+    public const string SHIELD_METAL3 = "FX_metallschild3";
+    public const string PUDDLE1 = "FX_pfuetze1";
+    public const string PUDDLE2 = "FX_pfuetze2";
+    public const string PUDDLE3 = "FX_pfuetze3";
+    public const string SHIP_CANON = "FX_schiffskanone";
+    public const string DOOR_OPEN = "FX_tuer oeffnen";
+    public const string WALL_TRIGGER = "FX_wandschalter";
+    public const string WATER_DROPS1 = "Tropfen1";
+    public const string WATER_DROPS2 = "Tropfen2";
+    public const string WATER_DROPS3 = "Tropfen3";
+    public const string WATER_DROPS4 = "Tropfen4";
+    public const string WATER_DROPS5 = "Tropfen5";
+
+    #endregion
+
+    #region Imp
+
+    public const string IMP_GOING = "FX_imp schlendern";
+    public const string IMP_SETUP_LADDER = "FX_leiter aufstellen";
+    public const string IMP_ATTACK1 = "FX_speerstich1";
+    public const string IMP_ATTACK2 = "FX_speerstich2";
+    public const string IMP_WORK = "Goblins_arbeit";
+    public const string IMP_DAMAGE = "Goblins_aua";
+    public const string IMP_DEATH = "Goblins_death";
+    public const string IMP_COMMENT1 = "Goblins_hach";
+    public const string IMP_COMMENT2 = "Goblins_hihihi";
+    public const string IMP_COMMENT3 = "Goblins_hmm";
+    public const string IMP_COMMENT4 = "Goblins_hmmhehe";
+    public const string IMP_SELECT1 = "Goblin_ich";
+    public const string IMP_SELECT2 = "Goblin_ ihr wuenscht";
+    public const string IMP_SELECT3 = "Goblin_ja 2";
+    public const string IMP_SELECT4 = "Goblin_ja meister";
+    public const string IMP_SELECT5 = "Goblin_ja";
+    public const string IMP_SELECT6 = "Goblin_jiaa";
+    public const string IMP_SELECT7 = "Goblins_mmmn 2";
+    public const string IMP_SELECT8 = "Goblins_mmmn";
+    public const string IMP_SELECT9 = "Goblins_was begehrt ihr";
+    public const string IMP_SELECT10 = "Goblins_was gibts 2";
+    public const string IMP_SELECT11 = "Goblins_was gibts";
+    public const string IMP_SELECT12 = "Goblins_was liegt an";
+
+    #endregion
+
+    #region Troll
+
+    public const string TROLL_ATTACK1 = "Troll_attack1";
+    public const string TROLL_ATTACK2 = "Troll_attack2";
+    public const string TROLL_ATTACK3 = "Troll_attack3";
+    public const string TROLL_DEATH = "Troll_death";
+    public const string TROLL_COMMENT1 = "Troll_draufhaun";
+    public const string TROLL_COMMENT2 = "Troll_oarr";
+    public const string TROLL_COMMENT3 = "Troll_oeoea";
+    public const string TROLL_COMMENT4 = "Troll_rahahaha";
+    public const string TROLL_COMMENT5 = "Troll_uooa";
+    public const string TROLL_COMMENT6 = "Troll_wooaa";
+
+    #endregion
+
+    #region Explosiver
+
+    public const string EXPLOSIVE_DEATH = "Explosiver_death";
+    public const string EXPLOSIVE_POTION_SOUND = "Explosiver_trankblubbern";
+    public const string EXPLOSIVE_COMMENT1 = "Explosiver_kaliumflourid";
+    public const string EXPLOSIVE_COMMENT2 = "Explosiver_lachen";
+    public const string EXPLOSIVE_COMMENT3 = "Explosiver_piff paff puff";
+    public const string EXPLOSIVE_COMMENT4 = "Explosiver_plutoniumammorgen";
+    public const string EXPLOSIVE_COMMENT5 = "Explosiver_uran, uran";
+
+    #endregion
+
+    #region Drachen
+
+    public const string DRAGON_ATTACK1 = "Dragon_attack1";
+    public const string DRAGON_ATTACK2 = "Dragon_attack2";
+    public const string DRAGON_ATTACK3 = "Dragon_attack3";
+    public const string DRAGON_WING1 = "Dragon_fluegel1";
+    public const string DRAGON_WING2 = "Dragon_fluegel2";
+    public const string DRAGON_DEATH = "Dragon_death";
+    public const string DRAGON_COMMENT1 = "Dragon_brrr";
+    public const string DRAGON_COMMENT2 = "Dragon_burrr";
+    public const string DRAGON_COMMENT3 = "Dragon_horr";
+    public const string DRAGON_COMMENT4 = "Dragon_mhm";
+    public const string DRAGON_COMMENT5 = "Dragon_mhmn";
+    public const string DRAGON_COMMENT6 = "Dragon_schnief1";
+    public const string DRAGON_COMMENT7 = "Dragon_schnief2";
+    public const string DRAGON_COMMENT8 = "Dragon_schnief3";
+    public const string DRAGON_COMMENT9 = "Dragon_woaah";
+    public const string DRAGON_COMMENT10 = "Dragon_woooah lang";
+
+    #endregion
+
+    #region Werwolf
+
+    public const string WERWOLF_ATTACK1 = "Werwolf_attack1";
+    public const string WERWOLF_ATTACK2 = "Werwolf_attack2";
+    public const string WERWOLF_HOWL = "Werwolf_heulen";
+    public const string WERWOLF_HUMAN_DAMAGE = "Werwolf_menschliches autsch";
+
+    #endregion
+
+    #region Wurm
+
+    public const string WORM_EAT1 = "Wurm_fressen";
+    public const string WORM_EAT2 = "Wurm_verschlingen";
+    public const string WORM_STOMACH1 = "Wurm_magen1";
+    public const string WORM_STOMACH2 = "Wurm_magen2";
+    public const string WORM_STOMACH3 = "Wurm_magen3";
+
+    #endregion
+
+    #region Koenig
+
+    public const string KING_ATTACK1 = "King_attack1";
+    public const string KING_ATTACK2 = "King_attack2";
+    public const string KING_ATTACK3 = "King_attack3";
+    public const string KING_DEATH = "King_death";
+    public const string KING_SPEECH = "King_bis zu mir";
+
+    #endregion
+
+}

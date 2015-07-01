b0ce43750acf4965daac077993358ae48bf40d72
djb12 <davidbrem@t-online.de>
2015-07-01 12:02:54 +0200

    created scripts for collectable items
    scripts have been created for single items and for whole collections

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/CollectableCollection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    class CollectableCollection : MonoBehaviour, CollectableObject.ICollectableObjectListener
+    {
+        private List<CollectableObject> collectionItems;
+
+        public void Awake()
+        {
+            collectionItems = GetComponentsInChildren<CollectableObject>().ToList();
+            collectionItems.ForEach(ci => ci.RegisterListener(this));
+        }
+
+        public void CollectItems()
+        {
+            collectionItems.ForEach(ci => ci.GetComponent<SpriteRenderer>().enabled = false);
+            collectionItems.ForEach(ci => ci.UnregisterListener(this));
+            collectionItems.ForEach(Destroy);
+        }
+
+        void CollectableObject.ICollectableObjectListener.OnCollected(CollectableObject self)
+        {
+            CollectItems();
+        }
+    }
+}

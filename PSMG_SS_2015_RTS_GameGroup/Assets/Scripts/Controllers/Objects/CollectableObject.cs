e7d0a9741a4361ae0349b995209d3970b91cf0e9
djb12 <davidbrem@t-online.de>
2015-07-01 10:39:10 +0200

    added bombs, spears and ladders to lvl 1

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/CollectableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    class CollectableObject : MonoBehaviour
+    {
+        // TODO
+    }
+}
---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/HighlightableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    class HighlightableObject : MonoBehaviour
+    {
+        // TODO
+    }
+}

4e6cc1d2156597db2166420485f77be4801ae5aa
djb12 <davidbrem@t-online.de>
2015-08-02 22:41:19 +0200

    added sprites for faces in speechbubble-messages

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/UIManagerAndServices/Speaker.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Managers.UIManagerAndServices
+{
+
+    public enum Speaker
+    {
+        Wilbur,
+        KruemelBart,
+        Knight,
+        Troll,
+        Koboldigunde
+    }
+}
--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/UIManagerAndServices/UIMessageService.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/UIManagerAndServices/UIMessageService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,16 @@ namespace Assets.Scripts.Managers.UIManagerAndServices
     {
 
         public const float SimpleTextMessageDuration = 5f;
+        public const float SpeechBubbleMessageDuration = 5f;
 
         public GameObject SimpleTextMessagePrefab;
         public GameObject SpeechBubbleMessagePrefab;
+
+        public Sprite WilburSprite;
+        public Sprite KruemelbartSprite;
+        public Sprite KnightSprite;
+        public Sprite TrollSprite;
+        public Sprite KoboldigundeSprite;
 
         public void CreateSimpleTextMessage(string message)
         {
@@ -32,17 +40,74 @@ namespace Assets.Scripts.Managers.UIManagerAndServices
             msg.transform.localPosition = new Vector3( // position message within canvas
                 pos.x - width / 6f, 
                 pos.y - height / 6f, 
-                pos.z);
+                pos.z
+                );
 
             yield return new WaitForSeconds(SimpleTextMessageDuration);
 
             Destroy(msg);
         }
 
-        public void CreateSpeechBubbleMessage(string message)
+        public void CreateSpeechBubbleMessage(string message, Speaker speaker)
         {
-            
+            StartCoroutine((SpeechBubbleMessageRoutine(message, speaker)));
         }
 
+        private IEnumerator SpeechBubbleMessageRoutine(string message, Speaker speaker)
+        {
+            var msg = Instantiate(SpeechBubbleMessagePrefab);
+
+            var text= 
+                msg.GetComponentsInChildren<Text>().ToList().First(t => t.gameObject.name == "Text");
+
+            text.text = message;
+
+            var speakerSpriteRenderer =
+                msg.GetComponentsInChildren<SpriteRenderer>().ToList().First(sr => sr.gameObject.name == "Speaker_Image");
+
+            var speakerLabel =
+               msg.GetComponentsInChildren<Text>().ToList().First(sr => sr.gameObject.name == "Speaker_Label");
+
+            switch (speaker)
+            {
+                case Speaker.Knight:
+                    speakerSpriteRenderer.sprite = KnightSprite;
+                    speakerLabel.text = "Knight";
+                    break;
+                case Speaker.Wilbur:
+                    speakerSpriteRenderer.sprite = WilburSprite;
+                    speakerLabel.text = "Wilbur";
+                    break;
+                case Speaker.Koboldigunde:
+                    speakerSpriteRenderer.sprite = KoboldigundeSprite;
+                    speakerLabel.text = "Koboldigunde";
+                    break;
+                case Speaker.KruemelBart:
+                    speakerSpriteRenderer.sprite = KruemelbartSprite;
+                    speakerLabel.text = "Kruemelbart";
+                    break;
+                case Speaker.Troll:
+                    speakerSpriteRenderer.sprite = TrollSprite;
+                    speakerLabel.text = "Troll";
+                    break;
+            }
+        
+            var canvas = GetComponent<UIManager>().CurrentUserInterface.UICanvas;
+            var pos = canvas.transform.position;
+            var width = canvas.GetComponent<RectTransform>().rect.width;
+            var height = canvas.GetComponent<RectTransform>().rect.height;
+
+            msg.transform.SetParent(canvas.transform, false); // set canvas as parent element
+
+            msg.transform.localPosition = new Vector3( // position message within canvas
+                pos.x - width / 6f,
+                pos.y - height / 6f,
+                pos.z
+                );
+
+            yield return new WaitForSeconds(SpeechBubbleMessageDuration);
+
+            Destroy(msg);
+        }
     }
 }

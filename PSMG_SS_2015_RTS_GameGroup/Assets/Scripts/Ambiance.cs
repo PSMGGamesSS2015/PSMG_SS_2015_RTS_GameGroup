0c6ef936fb6cd39134fe1f2d7f68fce658b1efba
djb12 <davidbrem@t-online.de>
2015-08-02 11:20:40 +0200

    created torch prefab
    - improved burning-torch animation
    - adapted ambiance-script
    - removed test prefabs

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Ambiance.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Ambiance.cs
@@ -1,40 +1,45 @@
 using UnityEngine;
-using System.Collections;
+#pragma warning disable 108,114
+
+// Adapted from the following source: http://answers.unity3d.com/questions/34739/how-to-make-a-light-flicker.html
  
-// Flickering light script 
+ namespace Assets.Scripts
+ {
+     public class Ambiance : MonoBehaviour {
  
- public class Ambiance : MonoBehaviour {
+         private Light light;
  
-     // Your light gameObject here.
-     public Light light;
- 
-     // Array of random values for the intensity.
-     private float[] smoothing = new float[20];
- 
-     void Start(){
-         // Initialize the array.
-         for(int i = 0 ; i < smoothing.Length ; i++){
-             smoothing[i] = .0f;
-         }
-     }
- 
-     void Update () {
-         float sum = .0f;
-         
-         // Shift values in the table so that the new one is at the
-         // end and the older one is deleted.
-         for(int i = 1 ; i < smoothing.Length ; i++)
+         private readonly float[] smoothing = new float[40];
+
+         public void Awake()
          {
-             smoothing[i-1] = smoothing[i];
-             sum+= smoothing[i-1];
+             light = GetComponent<Light>();
          }
+
+         public void Start()
+         {
+             for(var i = 0 ; i < smoothing.Length ; i++){
+                 smoothing[i] = .0f;
+             }
+         }
+
+         public void Update()
+         {
+             var sum = .0f;
+         
+             // Shift values in the table so that the new one is at the end and the older one is deleted.
+             for(var i = 1 ; i < smoothing.Length ; i++)
+             {
+                 smoothing[i-1] = smoothing[i];
+                 sum+= smoothing[i-1];
+             }
  
-         // Add the new value at the end of the array.
-         smoothing[smoothing.Length -1] = Random.value;
-         sum+= smoothing[smoothing.Length -1];
+             // Add the new value at the end of the array.
+             smoothing[smoothing.Length -1] = Random.value;
+             sum+= smoothing[smoothing.Length -1];
  
-         // Compute the average of the array and assign it to the
-         // light intensity.
-         light.intensity = sum / smoothing.Length;
+             // Compute the average of the array and assign it to the light intensity.
+             light.intensity = sum / smoothing.Length;
+         }
      }
  }

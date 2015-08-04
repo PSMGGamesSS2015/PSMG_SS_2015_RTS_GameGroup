a74fc5e62d57b5d07ecd7f496ca5c6d6f0e29307
guj10033 <j.guegel@gmail.com>
2015-08-04 03:33:40 +0200

    animation references cleared up
    removed double

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/AnimationReferences.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/AnimationReferences.cs
@@ -8,22 +8,27 @@
         public const string ImpWalkingSpear = "Imp Walking with Spear";
         public const string ImpWalkingLadder = "Imp Walking with Ladder";
         public const string ImpWalkingBomb = "Imp Walking with Bomb";
+
         public const string ImpTakingObject = "Imp Taking Object";
         public const string ImpAttackingWithSpear = "Imp Attacking with Spear";
         public const string ImpHidingBehindShield = "Imp Hiding Behind Shield";
         public const string ImpPlacingLadderHorizontally = "Imp Placing Ladder Horizontally";
         public const string ImpPlacingLadderVertically = "Imp Placing Ladder Vertically";
-        public const string ImpStandingWithSpear = "Imp Standing with Spear";
+
+        public const string ImpClimbingLadderSpearman = "Imp Climbing Ladder with Spear";     
         public const string ImpClimbingLadderUnemployed = "Imp Climbing Ladder";
+
         public const string ImpDetonatingBomb = "Explosion";
-        public const string ImpSelected = "Selection";
-        public const string ImpClimbingLadderSpearman = "Imp Climbing Ladder with Spear";
-        public const string ImpStandingWithShield = "Imp Standing with Shield";
-        public const string ImpDead = "Imp Dead";
+        public const string ImpSelected = "Selection";     
+
         public const string ImpStanding = "Imp Standing";
-        public const string ImpStandingWithSpear = "Imp Standing Spear";
+        public const string ImpStandingWithShield = "Imp Standing with Shield";
+        public const string ImpStandingWithSpear = "Imp Standing with Spear";
         public const string ImpStanding2 = "Imp Standing 2";
+ 
         public const string ImpTalk = "Imp Talk";
+        public const string ImpDead = "Imp Dead";
+
         public const string ImpWin = "Imp Win";
         public const string ImpWinLoop = "Imp Win Loop";
 

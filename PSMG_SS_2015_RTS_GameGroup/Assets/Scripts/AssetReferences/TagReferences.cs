c2ab15f80ead79bbb3f5751ddf1a409eecc5da64
djb12 <davidbrem@t-online.de>
2015-08-08 19:59:31 +0200

    created gaslight controller

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/TagReferences.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/TagReferences.cs
@@ -48,5 +48,7 @@
         public const string IntroTextLine = "IntroTextLine";
         public const string Paragraph = "Paragraph";
         public const string IntroText = "IntroText";
+
+        public const string Gaslight = "Gaslight";
     }
 }
---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/GaslightController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class GaslightController : MonoBehaviour
+    {
+        public bool IsLight;
+
+        private Light lightSource;
+        
+        public void Awake()
+        {
+            lightSource = GetComponentInChildren<Light>();
+
+            if (IsLight) Light();
+        }
+
+        public void Light()
+        {
+            IsLight = true;
+            lightSource.enabled = true;
+        }
+
+        public void GoOut()
+        {
+            IsLight = false;
+            lightSource.enabled = false;
+        }
+    }
+}

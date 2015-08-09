5e835e364f3a004b91564aae8309166ad80b0e82
djb12 <davidbrem@t-online.de>
2015-08-09 21:59:54 +0200

    adding buzzwasp scripts (unfinished)

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Characters/Enemies/BuzzWasp/BuzzWaspController.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Controllers.Characters.Enemies.BuzzWasp.Subservices;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Characters.Enemies.BuzzWasp
+{
+    public class BuzzWaspController : MonoBehaviour
+    {
+        public void Awake()
+        {
+            gameObject.AddComponent<BuzzWaspMovementService>();
+        }
+    }
+}
---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Characters/Enemies/BuzzWasp/Subservices/BuzzWaspMovementService.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Helpers;
+
+namespace Assets.Scripts.Controllers.Characters.Enemies.BuzzWasp.Subservices
+{
+    public class BuzzWaspMovementService : MovingObject
+    {
+        public override void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void FixedUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

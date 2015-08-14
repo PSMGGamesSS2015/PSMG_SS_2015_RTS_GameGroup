4f2512a624ff6ba29f5693136d86fdf81ec9927d
djb12 <davidbrem@t-online.de>
2015-08-14 10:57:19 +0200

    created FieryCakeController

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FieryCakeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.AssetReferences;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class FieryCakeController : MonoBehaviour
+    {
+        private List<FireController> flames;
+        private bool hasBeenExtinguished;
+
+        public void Awake()
+        {
+            hasBeenExtinguished = false;
+            flames = GetComponentsInChildren<FireController>().ToList();
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag != TagReferences.Imp) return;
+
+            if (!hasBeenExtinguished)
+            {
+                LightFires();
+            }
+        }
+
+        private void LightFires()
+        {
+            flames.ForEach(f => f.gameObject.SetActive(true));
+        }
+
+        public void Extinguish()
+        {
+            flames.ForEach(f => f.gameObject.SetActive(false));
+        }
+    }
+}

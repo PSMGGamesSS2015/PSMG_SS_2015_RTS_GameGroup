013af0c9f6448c87a30af10e65875fbc087274ac
djb12 <davidbrem@t-online.de>
2015-08-14 09:47:08 +0200

    created FireController

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FireController.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FireController.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.AssetReferences;
+using UnityEngine;
 
 namespace Assets.Scripts.Controllers.Objects
 {
@@ -7,14 +11,51 @@ namespace Assets.Scripts.Controllers.Objects
         public int OrderInSortingLayer;
         public string SortingLayerName;
 
+        private List<SpriteRenderer> redFlameTongues;
+        private List<SpriteRenderer> orangeFlameTongues;
+        private List<SpriteRenderer> yellowFlameTongues;
+
         public void Awake()
         {
-            // Get components
+            if (SortingLayerName == "") return;
+
+            redFlameTongues =
+                GetComponentsInChildren<SpriteRenderer>()
+                    .Where(sr => sr.tag == TagReferences.RedFlameTongue)
+                    .ToList();
+
+            orangeFlameTongues =
+               GetComponentsInChildren<SpriteRenderer>()
+                   .Where(sr => sr.tag == TagReferences.OrangeFlameTongue)
+                   .ToList();
+
+            yellowFlameTongues =
+               GetComponentsInChildren<SpriteRenderer>()
+                   .Where(sr => sr.tag == TagReferences.YellowFlameTongue)
+                   .ToList();
         }
 
         public void Start()
         {
-            // Do Layer stuff
+            if (SortingLayerName == "") return;
+
+            foreach (var ft in redFlameTongues)
+            {
+                ft.sortingLayerName = SortingLayerName;
+                ft.sortingOrder = OrderInSortingLayer;
+            }
+
+            foreach (var ft in orangeFlameTongues)
+            {
+                ft.sortingLayerName = SortingLayerName;
+                ft.sortingOrder = OrderInSortingLayer + 1;
+            }
+
+            foreach (var ft in yellowFlameTongues)
+            {
+                ft.sortingLayerName = SortingLayerName;
+                ft.sortingOrder = OrderInSortingLayer + 2;
+            }
         }
 
     }

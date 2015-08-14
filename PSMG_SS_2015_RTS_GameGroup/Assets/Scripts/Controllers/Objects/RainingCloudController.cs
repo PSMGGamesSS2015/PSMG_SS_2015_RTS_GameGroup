56c6d232d20eeefb5d6ff90ed293ceb65b38c1a8
djb12 <davidbrem@t-online.de>
2015-08-14 10:37:14 +0200

    added rain to rain cloud prefab

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/RainingCloudController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class RainingCloudController : MonoBehaviour
+    {
+
+        private GameObject rain;
+
+        public void Awake()
+        {
+            rain = GetComponent<ParticleSystem>().gameObject;
+        }
+
+        public void StartRaining()
+        {
+            rain.SetActive(true);
+        }
+
+        public void StopRaining()
+        {
+            rain.SetActive(false);
+        }
+
+    }
+}

be13c82057c2ed38c996dfb2dd5b2f6af3c2aec7
djb12 <davidbrem@t-online.de>
2015-08-19 08:28:53 +0200

    addded fire particle system controller

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FireParticleSystemController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.AssetReferences;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class FireParticleSystemController : MonoBehaviour
+    {
+        private List<ParticleSystem> particleSystems;
+
+        public void Awake()
+        {
+            particleSystems = GetComponentsInChildren<ParticleSystem>().ToList();
+        }
+
+        public void Start()
+        {
+            particleSystems.ForEach(ps => ps.GetComponent<Renderer>().sortingLayerName = SortingLayerReferences.MiddleForeground);
+        }
+    }
+}

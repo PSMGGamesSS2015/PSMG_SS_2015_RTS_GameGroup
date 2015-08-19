5e702cf4eaed91316a0d1aa415cfe5d7c2541679
djb12 <davidbrem@t-online.de>
2015-08-19 15:58:18 +0200

    fix: fiery cake controllre would spawn multiple fires

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FieryCakeController.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FieryCakeController.cs
@@ -7,23 +7,28 @@ namespace Assets.Scripts.Controllers.Objects
 {
     public class FieryCakeController : MonoBehaviour
     {
+
+        public bool HasBeenLit { get; private set; }
         public bool HasBeenExtinguished { get; private set; }
+
         private List<GameObject> fire; 
 
         public void Awake()
         {
             HasBeenExtinguished = false;
+            HasBeenLit = false;
         }
 
         public void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.tag != TagReferences.Imp) return;
 
-            if (!HasBeenExtinguished)
-            {
-                LightFires();
-            }
+            if (HasBeenExtinguished || HasBeenLit) return;
+
+            LightFires();
+            HasBeenLit = true;
         }
+
 
         private void LightFires()
         {
@@ -33,8 +38,8 @@ namespace Assets.Scripts.Controllers.Objects
 
         public void Extinguish()
         {
-            fire.ForEach(f => f.GetComponent<FireParticleSystemController>().Extinguish());
             HasBeenExtinguished = true;
+            fire.ForEach(f => f.GetComponent<FireParticleSystemController>().Extinguish());
         }
     }
 }

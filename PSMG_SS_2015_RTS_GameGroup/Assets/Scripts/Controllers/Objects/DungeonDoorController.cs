95a4d57f47e36047dc73c245375fa089b6035259
djb12 <davidbrem@t-online.de>
2015-08-20 19:56:39 +0200

    created dungeon-door prefab

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/DungeonDoorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class DungeonDoorController : MonoBehaviour
+    {
+        private SpriteRenderer doorOpenSprite;
+        private SpriteRenderer doorClosedSprite;
+
+        private const string DoorOpenName = "DoorOpen";
+        private const string DoorClosedName = "DoorClosed";
+
+        public DoorState State { get; private set; }
+
+        public enum DoorState
+        {
+            Open,
+            Closed
+        }
+
+        public void Awake()
+        {
+            doorOpenSprite = GetComponentsInChildren<SpriteRenderer>().ToList().First(sr => sr.name == DoorOpenName);
+            doorClosedSprite = GetComponentsInChildren<SpriteRenderer>().ToList().First(sr => sr.name == DoorClosedName);
+
+            State = DoorState.Closed;
+        }
+
+        public void Open()
+        {
+            GetComponent<Collider2D>().enabled = false;
+
+            doorClosedSprite.enabled = false;
+            doorOpenSprite.enabled = true;
+
+            State = DoorState.Open;
+        }
+
+        public void Close()
+        {
+            GetComponent<Collider2D>().enabled = true;
+
+            doorOpenSprite.enabled = true;
+            doorClosedSprite.enabled = false;
+
+            State = DoorState.Closed;
+        }
+
+    }
+}

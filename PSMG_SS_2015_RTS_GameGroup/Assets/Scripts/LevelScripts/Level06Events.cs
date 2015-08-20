3c71547582526558dd863b752f6560472ea737b2
djb12 <davidbrem@t-online.de>
2015-08-20 17:38:24 +0200

    adding texts for level 06 events

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/LevelScripts/Level06Events.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/LevelScripts/Level06Events.cs
@@ -5,33 +5,43 @@ namespace Assets.Scripts.LevelScripts
 {
     public class Level06Events : LevelEvents
     {
-
         private Event levelStartedMessage;
-        private Event closedDoorMessage;
-        private Event torchesCollectedMessage;
-        private Event potionCollectedMessage;
-        private Event canonFiringMessage;
+        private Event flourHasFallenIntoBowlMessage;
+        private Event cakeAlmostReadyMessage;
+        private Event cakeReadyMessage;
+        private Event knightEatingCakeMessage;
+        private Event impsAreAtSuspensionBridgeMessage;
+        private Event impsHaveRescuedPrincessMessage;
+        private Event kingJumpsOutOfCakeMessage;
 
         protected override void RegisterEvents()
         {
             var narrator = SoundManager.Instance.Narrator;
 
             levelStartedMessage = events.First(e => e.Nr == 1);
-            levelStartedMessage.Message = " Mhh, hier geht’s nicht weiter, mein Herr. Wir müssen einen anderen Weg finden!";
+            levelStartedMessage.Message = "Meister, sprengt dieses lästige Tor aus dem Weg.";
 
-            closedDoorMessage = events.First(e => e.Nr == 2);
-            closedDoorMessage.Message = "Wir haben einen Haufen Leitern gefunden.";
+            flourHasFallenIntoBowlMessage = events.First(e => e.Nr == 2);
+            flourHasFallenIntoBowlMessage.Message = "Mmh, es ist Mehl in die Schüssel gefallen. Mit Wasser und etwas zum Rühren könnten wir uns einen Kuchen backen.";
 
-            torchesCollectedMessage = events.First(e => e.Nr == 3);
-            torchesCollectedMessage.Message = "Fackeln… guuut. Sie brennen hell und heiß. Die werden wir noch brauchen!";
+            cakeAlmostReadyMessage = events.First(e => e.Nr == 3);
+            cakeAlmostReadyMessage.Message = "Das riecht fast so gut wie die Leckereien der Prinzessin, mein Herr. Er muss nur noch kurz backen.";
 
-            potionCollectedMessage = events.First(e => e.Nr == 4);
-            potionCollectedMessage.Message = "Sieht aus wie eine Art Zaubertrank. Ein kleines Wildschwein ist auf der Flasche abgebildet. (Grunz Grunz). Laut Etikett soll er übermenschliche Kräfte verleihen!";
+            cakeReadyMessage = events.First(e => e.Nr == 4);
+            cakeReadyMessage.Message = "Mmmmmhh.";
 
-            canonFiringMessage = events.First(e => e.Nr == 5);
-            canonFiringMessage.Message = "Achtung, wir stehen unter Feueeer!!! In Deckung!";
+            knightEatingCakeMessage = events.First(e => e.Nr == 5);
+            knightEatingCakeMessage.Message = "Kuchen … hmm … nur ein kleines Stück.";
+            // TODO knightEatingCakeMessage.Message = "Lasst uns schnell weitergehen, bevor der Ritter aufgegessen hat.";
+
+            impsAreAtSuspensionBridgeMessage = events.First(e => e.Nr == 6);
+            impsAreAtSuspensionBridgeMessage.Message = "Wo ist bloß die Prinzessin? Wir konnten sie nirgendwo finden. Dringt tiefer ins Verlies vor. Versucht, diese Brücke zu Fall zu bringen.";
+
+            impsHaveRescuedPrincessMessage = events.First(e => e.Nr == 7);
+            impsHaveRescuedPrincessMessage.Message = "Geschafft, geschafft, geschafft! Nun bringt die Prinzessin sicher aus dem Verlies.";
+
+            kingJumpsOutOfCakeMessage = events.First(e => e.Nr == 8);
+            kingJumpsOutOfCakeMessage.Message = "Ihr wollt meine Dienerin entführen? Niemand klaut mir meine Bäckerin!";
         }
-
-
     }
 }

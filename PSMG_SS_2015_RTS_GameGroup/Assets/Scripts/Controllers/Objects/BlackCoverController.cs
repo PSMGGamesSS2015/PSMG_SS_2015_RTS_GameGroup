aa64d1f4ad9c7c432eef2ecba4fb6165c88466d2
djb12 <davidbrem@t-online.de>
2015-08-23 10:52:58 +0200

    added black cover to BackStuebchen

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/BlackCoverController.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.AssetReferences;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class BlackCoverController : MonoBehaviour
+    {
+        public void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.gameObject.tag == TagReferences.Imp)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

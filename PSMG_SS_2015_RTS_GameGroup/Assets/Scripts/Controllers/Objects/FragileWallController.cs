87723adddd9a9f57bdc6baae89a6d8873a9efee3
djb12 <davidbrem@t-online.de>
2015-08-27 23:47:53 +0200

    created fragile-wall prefab

---
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Controllers/Objects/FragileWallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Objects
+{
+    public class FragileWallController : MonoBehaviour
+    {
+        private List<Rigidbody2D> stones; 
+
+        public void Awake()
+        {
+            stones = GetComponentsInChildren<Rigidbody2D>().ToList();
+        }
+
+        public void Detonate()
+        {
+            // TODO make rigidbodies non-kinematic
+
+            // TODO apply force
+        }
+    }
+}

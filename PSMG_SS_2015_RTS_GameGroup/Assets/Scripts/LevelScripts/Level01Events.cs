3e0e32e0b7ae8230971092196d22431912c4f9d8
djb12 <davidbrem@t-online.de>
2015-08-27 21:26:54 +0200

    replaced old texts in Level01Events

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/LevelScripts/Level01Events.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/LevelScripts/Level01Events.cs
@@ -1,6 +1,6 @@
 ﻿using System.Linq;
-using Assets.Scripts.Managers;
 using Assets.Scripts.AssetReferences;
+using Assets.Scripts.Managers;
 
 namespace Assets.Scripts.LevelScripts
 {
@@ -15,40 +15,62 @@ namespace Assets.Scripts.LevelScripts
         private Event weaponsCollectedMessage;
         private Event bombNeededMessage;
         private Event trollMetMessage;
+        private Event professionAssignedMessage;
+        private Event suspensionBridgesCrossed;
+        private Event levelCompleted;
 
         protected override void RegisterEvents()
         { 
             mapStartetMessage = Events.First(e => e.Nr == 1);
-            mapStartetMessage.Message = "Macht euch auf die Socken! Die Prinzessin wurde entführt.";
+            mapStartetMessage.Message = "Gebieter, Prinzessin Koboldigunde wurde entführt. König Krümelbart hat sie in sein tiefstes Verlies geworfen. Ihr müsst sie retten. Macht euch auf!!!";
             mapStartetMessage.Action = MapStartetAction;    
 
             shieldCarrierBlockingMessage = Events.First(e => e.Nr == 2);
-            shieldCarrierBlockingMessage.Message = "Feiglinge verschanzen sich hinter ihren dicken Holzschilden. Daran kommt man nicht so leicht vorbei";
+            shieldCarrierBlockingMessage.Message = "Mein Herr. Ihr könnt Kobolde auswählen, indem ihr auf sie klickt oder die Tab-Taste benutzt.";
             shieldCarrierBlockingMessage.Action = ShieldCarrierBlockingAction;
-           
+
+            // TODO not played so far; should be played when the first imps is selected; create
+            professionAssignedMessage = Events.First(e => e.Nr == 9);
+            professionAssignedMessage.Message = "Herrvorragend. Nun gebt mir was zu tun!";
+
+            // TODO should be played when first coward is assigned
             shieldCarrierHeavyMessage = Events.First(e => e.Nr == 3);
-            shieldCarrierHeavyMessage.Message = "Unsere Schilde sind wuchtig und schwer. Ob die Brücke das aushält?";
+            shieldCarrierHeavyMessage.Message = "Hierhinter bin ich sicher, Meister. Nichts und niemand kommt an mir vorbei.";
             shieldCarrierHeavyMessage.Action = ShieldCarrierHeavyAction;
 
+            // TODO created
+            suspensionBridgesCrossed = Events.First(e => e.Nr == 10);
+            suspensionBridgesCrossed.Message =
+                "Diese Hängebrücke sieht zerbrechlich aus, Gebieter. Ich weiß nicht, ob sie das Gewicht unserer Schilde trägt.";
+
             laddersNeededMessage = Events.First(e => e.Nr == 4);
-            laddersNeededMessage.Message = "Legt Leitern über die Abgründe, um auf die andere Seite zu kommen.";
+            laddersNeededMessage.Message = "Mein Herr, eure Kobolde brauchen Leitern, um Abgründe zu überqueren.";
             laddersNeededMessage.Action = LaddersNeededAction;
 
             laddersCollectedMessage = Events.First(e => e.Nr == 5);
-            laddersCollectedMessage.Message = "Wir haben einen Haufen Leitern gefunden.";
+            laddersCollectedMessage.Message = "Meisterlich, Meister. Wir können die Leitern nutzen, um Hindernisse zu überqueren.";
             laddersCollectedMessage.Action = LaddersCollectedAction;
 
             bombNeededMessage = Events.First(e => e.Nr == 6);
-            bombNeededMessage.Message = "Der Fels ist brüchig. Mit einer Bombe können wir in wegsprengen.";
+            bombNeededMessage.Message = "Ein dicker, fetter Brocken. Sprengt ihn, mein Herr. In unserer Waffenkammer liegen Bomben und Speere.";
             bombNeededMessage.Action = BombNeededAction;
 
             weaponsCollectedMessage = Events.First(e => e.Nr == 7);
-            weaponsCollectedMessage.Message = "Pieks pieks, bumm bumm.";
+            weaponsCollectedMessage.Message = "Sprengstoff und Speere. Nun sind wir gerüstet. Reißt alles nieder! Fels und Mauer, Tor und Turm.";
             weaponsCollectedMessage.Action = WeaponsCollectedAction;
 
             trollMetMessage = Events.First(e => e.Nr == 8);
-            trollMetMessage.Message = "Ein Troll! Vorsichtig, die sind stark und schnell sauer!";
+            trollMetMessage.Message = "Ein Troll … Achtsam, mein Herr. Diese Ungeheuer sind stark und rasch verärgert. Lasst uns aus der Deckung heraus angreifen, um das Biest auszuschalten";
             trollMetMessage.Action = TrollMetAction;
+
+            levelCompleted = Events.First(e => e.Nr == 11);
+            levelCompleted.Message = "Hmmm, auf dem Weg liegen Kuchenkrümel herum. Die Prinzessin muss hier vorbeigekommen sein. Ihr habt die Spur des arglistigen Entführers gefunden. Ein Meisterstück, eure Boshaftigkeit.";
+            levelCompleted.Action = LevelCompletedAction;
+        }
+
+        private void LevelCompletedAction()
+        {
+            // TODO
         }
 
         private void TrollMetAction()

dc0c6eded9440a70538c38266a61186031ebb5b4
djb12 <davidbrem@t-online.de>
2015-08-28 12:46:22 +0200

    removing unused scenes from build

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/SceneReferences.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/AssetReferences/SceneReferences.cs
@@ -5,11 +5,8 @@
         #region InGameScenes
 
         public const string LevelXXKoboldingenReturn = "Level_XX_Koboldingen_Return";
-        public const string Level07CastleGlazeBreakout = "Level_07_CastleGlaze_Breakout";
         public const string Level06CastleGlazeDungenon = "Level_06_CastleGlaze_Dungeon";
         public const string Level05CastleGlazeArrival = "Level_05_CastleGlaze_Arrival";
-        public const string Level04TrollVillage = "Level_04_CinnamonWood_TrollVillage";
-        public const string Level03CinnamonWood = "Level_03_CinnamonWood";
         public const string Level02CherryTopMountains = "Level_02_CherryTopMountains";
         public const string Level01Koboldingen = "Level_01_Koboldingen";
         
@@ -19,7 +16,6 @@
 
         public const string LoadingScreen = "LoadingScreen";
         public const string MainMenu = "MainMenu";
-        public const string TestScene = "Test Level";
 
         #endregion
 

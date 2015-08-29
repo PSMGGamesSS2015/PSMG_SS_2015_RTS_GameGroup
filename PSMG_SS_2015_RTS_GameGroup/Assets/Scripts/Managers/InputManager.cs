0b01e07656af69899b02f2aca164c16b050a8ce6
djb12 <davidbrem@t-online.de>
2015-08-29 12:11:27 +0200

    disabling input manager in non ingame lvl

--- PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/InputManager.cs
+++ PSMG_SS_2015_RTS_GameGroup/Assets/Scripts/Managers/InputManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Assets.Scripts.Config;
 using Assets.Scripts.Controllers.Objects;
 using Assets.Scripts.ParameterObjects;
 using Assets.Scripts.Types;
@@ -46,6 +47,8 @@ namespace Assets.Scripts.Managers
 
         public void OnGUI()
         {
+            if (LevelManager.Instance.CurrentLevel.CurrentLevelConfig.Type != LevelConfig.LevelType.InGame) return;
+
             var e = Event.current;
             CheckScrollInput(e);
             CheckKeyInput(e);
